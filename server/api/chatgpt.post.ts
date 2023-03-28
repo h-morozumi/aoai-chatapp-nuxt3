ca90c948e39d8d00c9234ee82b79ac9e5e7237b7
MOROZUMI, Hiroyuki <113565981+h-morozumi@users.noreply.github.com>
2023-03-28 03:09:03 +0000

    APIをリファクタ

--- server/api/chatgpt.post.ts
+++ server/api/chatgpt.post.ts
@@ -3,12 +3,14 @@ const runtimeConfig = useRuntimeConfig();
 const endpoint = runtimeConfig.aoaiEndpoint;
 const deployname = runtimeConfig.aoaiDeployname;
 const apiKey = runtimeConfig.aoaiApikey;
+const baseUrl = `${endpoint}openai/deployments/${deployname}/chat/completions?api-version=2023-03-15-preview`;
 
 // Debug log
-console.log('環境変数');
-console.log(endpoint);
-console.log(deployname);
-console.log(apiKey);
+console.log('---環境変数---');
+console.log(`エンドポイント:${endpoint}`);
+console.log(`デプロイ名:${deployname}`);
+console.log(`APIキー:${apiKey}`);
+console.log(`接続先URL:${baseUrl}`);
 
 /**
  * ChatGPTのメッセージ型
@@ -18,52 +20,55 @@ interface Message {
   content: string;
 }
 
+const debugLog = (message: string, data?: unknown) => {
+  console.log(`${message}:`, data);
+};
+
 /**
  * AOAI の ChatGPTにメッセージを送信する
  */
 export default defineEventHandler(async (event) => {
-  //パラメータを取得
+  // リクエストボディから必要な情報を取得
   const body = await readBody(event);
-  
-  // Systemメッセージ
   const systemMessage:string = body.systemMessage;
-  // 過去～今回のユーザメッセージ
   const userMessage:Message[] = body.userMessage;
-  // メッセージを組み立て
-  var messages:Message[]  = [];
-  messages.push({role : 'system',content : systemMessage});
-  messages = messages.concat(userMessage);
 
-  // debug log
-  console.log('送信予定メッセージの内容');
-  console.log(messages);
+  // パラメータバリデーション
+  if (!Array.isArray(userMessage)) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'userMessage is not an array',
+    });
+  }
 
-  //URLを組み立て
-  const url = endpoint + 'openai/deployments/' + deployname + '/chat/completions?api-version=2023-03-15-preview';
+  const messages: Message[] = [
+    { role: 'system', content: systemMessage },
+    ...userMessage,
+  ];
 
-  // debug log
-  console.log('送信先URL');
-  console.log(url);
+  debugLog('送信予定メッセージの内容', JSON.stringify(messages,undefined,4));
 
   try {
-    const ret = await $fetch(url,{
-      method : 'POST',
-      headers : {
-        'Content-Type': 'application/json',
-        'api-key': apiKey,
-      },
-      body : {
-        messages : messages
-      }
+    const response = await $fetch(baseUrl,{
+      method: 'POST',
+      headers: Object.assign(
+        { 'Content-Type': 'application/json', 'api-key': apiKey },
+        {}
+      ),
+      body: JSON.stringify({ messages }),
     });
 
     // Debug log
-    console.log('取得データ');
-    console.log(ret);
+    debugLog('取得データ', JSON.stringify(response,undefined,4));
 
-    return ret;
-  } catch (error) {
-    console.log(error);
-    throw new Error('AOAIへの送信に失敗しました。');
+    //そのまま返す
+    return response;
+    
+  } catch (e) {
+    debugLog('エラーが発生しました', e);
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'AOAI接続時にエラーが発生しました',
+    })
   }
 });
