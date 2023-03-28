5a1f0925273131a3a90d7524eed1a59e64c29196
MOROZUMI, Hiroyuki <113565981+h-morozumi@users.noreply.github.com>
2023-03-28 08:06:11 +0000

    左ペインいったん作成

--- pages/tailwind.vue
+++ pages/tailwind.vue
@@ -3,27 +3,30 @@
   <div class="flex flex-row ">
     <div class="basis-1/2 min-h-screen m-5">
       <div class="bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
-      <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
+      <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Playground</h2>
+      <p class="leading-relaxed mb-5 text-gray-600">Azure OpenAI ChatGPT を使ったデモアプリケーション</p>
       <div class="relative mb-4">
-        <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-        <input type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+        <label for="name" class="leading-7 text-lg text-gray-600">アシスタント</label>
+        <p class="text-xs text-gray-500">モデルの動作と、応答を生成するときに参照する必要があるコンテキストに関する指示をモデルに与えます。アシスタントの性格を説明し、回答すべきことと回答すべきでないことを伝え、回答の形式を説明することができます。このセクションにはトークンの制限はありませんが、すべての API 呼び出しに含まれるため、全体的なトークンの制限に対してカウントされます。</p>
+        <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-40 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+        <button class="text-white bg-indigo-500 border-0 py-2 px-2 focus:outline-none hover:bg-indigo-600 rounded text-lg">保存</button>
       </div>
-      <div class="relative mb-4">
+      <!-- <div class="relative mb-4">
         <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
         <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
-      </div>
+      </div> -->
       <div class="relative mb-4">
-        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-        <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+        <label for="message" class="leading-7 text-lg text-gray-600">Message</label>
+        <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-40 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
       </div>
-      <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-      <p class="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
+      <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">送信</button>
+      <p class="text-xs text-gray-500 mt-3">Azure OpenAI ChatGPTを使ったデモ。現在 gpt-35-turbo (version 0301)にて検証中</p>
     </div>
     </div>
-    <div class="basis-1/2 bg-sky-500/75 min-h-screen m-5">
+
+    <div class="basis-1/2 bg-sky-500/75 m-5">
       <div>
-  <section class="text-gray-600 body-font overflow-hidden">
+  <section class="text-gray-600 body-font overflow-auto">
   <div class="container px-5 py-24 mx-auto">
     <div class="-my-8 divide-y-2 divide-gray-100">
       <div class="py-8 flex flex-wrap md:flex-nowrap">
