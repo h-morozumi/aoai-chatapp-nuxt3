e77875e4570a31c1b4fc0218294d4248c7cb0230
h-morozumi <morozumi.h@outlook.com>
2023-04-03 19:07:07 +0900

    入力補助を追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,15 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-      class="px-3"
-      color="white"
-      flat
-      density="compact"
-    >
-      <v-avatar
-        color="grey-darken-1"
-        size="32"
-      ></v-avatar>
-
+    <v-app-bar class="px-3" color="white" flat density="compact">
       <v-spacer></v-spacer>
 
-      <v-tabs
-        centered
-        color="grey-darken-2"
-      >
-        <v-tab
-          v-for="link in links"
-          :key="link"
-        >
-          {{ link }}
-        </v-tab>
+      <v-tabs centered color="grey-darken-2">
+        <v-tab to="/">Home</v-tab>
+        <v-tab to="/about">About</v-tab>
+        <v-tab to="/sample">Sample</v-tab>
       </v-tabs>
       <v-spacer></v-spacer>
 
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey-darken-1"
-        size="32"
-      ></v-avatar>
     </v-app-bar>
 
     <v-main class="bg-grey-lighten-3">
@@ -39,12 +18,7 @@
   </v-app>
 </template>
 <script setup>
-  const links = [
-    'Dashboard',
-    'Messages',
-    'Profile',
-    'Updates',
-  ]
+
 </script>
 
 <style scoped>
--- pages/about.vue
+++ pages/about.vue
@@ -1,45 +1,171 @@
 <template>
       <v-container>
         <v-row>
-          <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
+          <v-col cols="12" sm="3">
+            <v-sheet rounded="lg" min-height="70vh">
               <!--  -->
+
+
             </v-sheet>
           </v-col>
 
-          <v-col
-            cols="12"
-            sm="8"
-          >
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
+          <v-col cols="12" sm="6">
+            <v-sheet rounded="lg" min-height="70vh">
               <!--  -->
+              <v-container ref="container" class="overflow-y-auto" style="height: 70vh">
+
+
+                <!-- GPT Message Template -->
+                <v-row>
+                  <v-col cols="12" sm="2">
+                    <v-avatar image="chatgpt-logo.jpg" color="grey lighten-4" size="40">
+                    </v-avatar>
+                  </v-col>
+                  <v-col cols="12" sm="10">
+                    <v-card class="mx-auto my-12" elevation="2" max-width="400" color="grey lighten-10">
+                      <v-card-text>
+                        Hello World.
+                      </v-card-text>
+                    </v-card>
+                  </v-col>
+                </v-row>
+                <!-- Message Template -->
+
+                <!-- User Message Template -->
+                <v-row>
+                  <v-col cols="12" sm="2">
+                    <v-avatar color="blue lighten-4" size="40">
+                      <v-icon>mdi-account</v-icon>
+                    </v-avatar>
+                  </v-col>
+                  <v-col cols="12" sm="10">
+                    <v-card class="mx-auto my-12" elevation="2" max-width="400" color="grey lighten-10">
+                      <v-card-text>
+                        Hello World.
+                      </v-card-text>
+                    </v-card>
+                  </v-col>
+                </v-row>
+                <!-- Message Template -->
+                <!-- Message Template -->
+                <v-row>
+                  <v-col cols="12" sm="2">
+                    <v-avatar color="grey lighten-4" size="40">
+                      <v-icon>mdi-account</v-icon>
+                    </v-avatar>
+                  </v-col>
+                  <v-col cols="12" sm="10">
+                    <v-card class="mx-auto my-12" elevation="2" max-width="400" color="grey lighten-10">
+                      <v-card-text>
+                        Hello World.
+                      </v-card-text>
+                    </v-card>
+                  </v-col>
+                </v-row>
+                <!-- Message Template -->
+
+                <!-- Message Template -->
+                <v-row>
+                  <v-col cols="12" sm="2">
+                    <v-avatar color="blue lighten-4" size="40">
+                      <v-icon>mdi-account</v-icon>
+                    </v-avatar>
+                  </v-col>
+                  <v-col cols="12" sm="10">
+                    <v-card class="mx-auto my-12" elevation="2" max-width="400" color="grey lighten-10">
+                      <v-card-text>
+                        Hello World.
+                      </v-card-text>
+                    </v-card>
+                  </v-col>
+                </v-row>
+                <!-- Message Template -->
+
+                <!-- Message Template -->
+                <v-row>
+                  <v-col cols="12" sm="2">
+                    <v-avatar color="grey lighten-4" size="40">
+                      <v-icon>mdi-account</v-icon>
+                    </v-avatar>
+                  </v-col>
+                  <v-col cols="12" sm="10">
+                    <v-card class="mx-auto my-12" elevation="2" max-width="400" color="grey lighten-10">
+                      <v-card-text>
+                        Hello World.
+                      </v-card-text>
+                    </v-card>
+                  </v-col>
+                </v-row>
+                <!-- Message Template -->
+
+
+
+
+
+
+
+
+
+
+
+              </v-container>
+
+
+
+
             </v-sheet>
           </v-col>
 
-          <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
+          <v-col cols="12" sm="3">
+            <v-sheet rounded="lg" min-height="70vh">
               <!--  -->
+
+
             </v-sheet>
           </v-col>
         </v-row>
+
+        <v-container>
+          <v-col cols="12" sm="12">
+            <v-card>
+              <v-card-text>
+                <v-row>
+                  <v-col>
+                    <!-- <v-text-field autofocus label="メッセージ ※Enterでも送信できるよ" v-model="message" clearable @keyup.enter="send_onClick"></v-text-field> -->
+                    <v-textarea auto-grow rows="1" prepend-icon="mdi-comment" counter autofocus label="メッセージ" v-model="message" clearable @keyup.enter="send_onClick"></v-textarea>
+                  </v-col>
+                </v-row>
+                <v-btn class="info" small @click="send_onClick">
+                  <v-icon>mdi-play</v-icon>送信
+                </v-btn>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-container>
+
       </v-container>
+
+
+
 </template>
 
 <script setup>
+  const message = ref("");
+  const messages = ref([]);
+  const container = ref('container');
 
+  const send_onClick = async (event) => {
+    // ShiftKeyが押されている場合は改行
+    if(event.shiftKey){
+      console.log("ShiftKey");
+      return;
+    }
+    console.log("send_onClick");
+    console.log(`ScrollTop: ${container.value.$el.scrollTop}`);
+    console.log(`ScrollHeight: ${container.value.$el.scrollHeight}`);
+    // 入力内容をクリア
+    message.value = "";
+    // チャット欄を最下部にスクロール
+    container.value.$el.scrollTop = container.value.$el.scrollHeight;
+  }
 </script>
